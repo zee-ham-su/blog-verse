10baeda2e19ce87258b2b2c043204b11d68a2672
zee-ham-su <hsufiian@yahoo.com>
2025-01-01 23:58:42 +0000

    Add configuration for environment variables and MongoDB connection; update server startup message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+
+  await app.listen(3000);
+  console.log('Server is running on port 🚀', 'http://localhost:3000');
 }
 bootstrap();
